a373f5fa0955937a25d72dbd1fec11d307787fc3
honor-zlc <1525590918@qq.com>
2020-12-31 10:20:48 +0800

    generate_demo

---
+++ src/test/java/com/example/generate_demo/GenerateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.generate_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerateDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
